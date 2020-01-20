1f0cb1f816bcff90b76dc40c9227ab709751aabe
tong <newrtong@gmail.com>
2020-01-20 13:17:19 +0800

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log('Typescript demo~');
+var message = 'TypeScript hello';
+function showMessage(words) {
+    console.log(words);
+}
+showMessage(message);
---
+++ index.ts
@@ -0,0 +1,8 @@
+console.log('Typescript demo~')
+
+const message = 'TypeScript hello'
+function showMessage(words: string): void {
+    console.log(words)
+}
+
+showMessage(message)
